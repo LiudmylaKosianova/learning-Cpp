3deca842b380d1f84ce7518d5e947fd1b8256199
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-05 08:00:44 +0200

    cpp institute

---
+++ cpp-institute/cpp-essentials/try.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+int main(void){
+
+    // int aInt = 5;
+    // float aFloat = 5E3;
+    // cout << sizeof(aInt) << endl;
+    // cout << aFloat << endl;
+
+
+
+    return 0;
+}
