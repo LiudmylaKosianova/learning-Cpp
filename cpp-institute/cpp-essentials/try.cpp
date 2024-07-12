4132e42b3102be6855b01aeb5cd61d2493979049
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-07-12 20:01:29 +0200

    small changes cpp-institute

--- cpp-institute/cpp-essentials/try.cpp
+++ cpp-institute/cpp-essentials/try.cpp
@@ -10,6 +10,21 @@ int main(void){
     // cout << sizeof(aInt) << endl;
     // cout << aFloat << endl;
 
+    //variants of the while loop
+    short count1 = 3, count2 = 3, count3 = 3;
+    
+
+    while(count1 != 0){
+        cout << "I am in a count1 loop!" << endl;
+        count1 --;
+    }
+    while(count2){
+        cout << "I am in a count2 loop!" << endl;
+        count2 --;
+    }
+    while(count3--){
+        cout << "I am in a count3 loop!" << endl;
+    }
 
 
     return 0;
