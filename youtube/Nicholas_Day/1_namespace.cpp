bd865542a6c7f537b72c292ec2a6559052d28c9a
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-07-21 13:08:33 +0200

    lecture one notes Nicholas Day youtube

--- youtube/Nicholas_Day/1_namespace.cpp
+++ youtube/Nicholas_Day/1_namespace.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+//using namespace std;
 
 namespace myTenA{
     int a = 10;
@@ -7,6 +9,8 @@ namespace myTenA{
 namespace mySevenA{
     int a = 7;    
 }
+
+typedef float DecimalPoint;
 
 int main(){
 
@@ -17,7 +21,38 @@ int main(){
     int c2 = a;
     std::cout << "c2 = " << c2 << std::endl;
 
+/* typedef */
+    DecimalPoint myFloat = 1.097;
+    std::cout << "myFloat = " << myFloat << std::endl;
 
+    /*explicit assignment*/
+
+    int age = 21;
+
+    /*implicit assignment*/
+
+    int age2(21);
+
+    /*strings*/
+
+    
+
+    std::string myWords = "It is a hot summer day";
+    char firstLetter = myWords[0];
+
+    /*constants*/
+    int age3;
+    age3 = 791;
+    const int RETIRE_AGE = 67;
+
+    std::cout << &age3 << std::endl;
+
+    int * ptr_age3 = &age3;
+
+    std::cout << ptr_age3 << std::endl;
+    std::cout << *ptr_age3 << std::endl;
+
+    
 
     return 0;
 }
