88dcf91db821eeb6293b01628576c9544b1a1e9f
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-07-25 09:04:22 +0200

    lecture 2 exercise 3 added switch

--- youtube/Nicholas_Day/lecture2/2_ex3.cpp
+++ youtube/Nicholas_Day/lecture2/2_ex3.cpp
@@ -13,8 +13,18 @@ int main(){
     enum m_s {stand, walk, run, crawl};
     srand(time(NULL));
     m_s state = (m_s)(rand()%4);
-    cout << state << "\n";
+    cout <<"State is " << state << "\n";
     
+    switch(state){
+        case 0: cout << "You are standing. Next you can walk (1) or crawl (3)\n";
+                break;
+        case 1: cout << "You are walking. Next you can stand (0) or run (2)\n";
+                break;
+        case 2: cout << "You are running. Next you can walk (1)\n";
+                break;
+        default:cout << "You are crawling. Next you can stand (0)\n";
+                break;
+    }
 
 
     return 0;
