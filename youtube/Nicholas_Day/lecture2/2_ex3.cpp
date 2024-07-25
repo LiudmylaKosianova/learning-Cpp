c25d9aa46b90d6fbe47870f89e01c09d12a5808c
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-07-25 09:25:32 +0200

    lecture 2 exercise 4 finished

--- youtube/Nicholas_Day/lecture2/2_ex3.cpp
+++ youtube/Nicholas_Day/lecture2/2_ex3.cpp
@@ -15,15 +15,34 @@ int main(){
     m_s state = (m_s)(rand()%4);
     cout <<"State is " << state << "\n";
     
-    switch(state){
-        case 0: cout << "You are standing. Next you can walk (1) or crawl (3)\n";
-                break;
-        case 1: cout << "You are walking. Next you can stand (0) or run (2)\n";
-                break;
-        case 2: cout << "You are running. Next you can walk (1)\n";
-                break;
-        default:cout << "You are crawling. Next you can stand (0)\n";
-                break;
+    // switch(state){
+    //     case 0: cout << "You are standing. Next you can walk (1) or crawl (3)\n";
+    //             break;
+    //     case 1: cout << "You are walking. Next you can stand (0) or run (2)\n";
+    //             break;
+    //     case 2: cout << "You are running. Next you can walk (1)\n";
+    //             break;
+    //     default:cout << "You are crawling. Next you can stand (0)\n";
+    //             break;
+    // }
+
+    for(int i = 0; i < 10; i++){
+         cout <<"State is " << state << "\n";
+    
+        switch(state){
+            case 0: cout << "You are standing. Next you can walk (1) or crawl (3)\n";
+                    state = (m_s)((rand()%2)*2+1); //rand() generates 0 or 1, then multiplied by 2 (0 or 2 respectively), then added 1 (1 or 3 - legal transitions)
+                    break;
+            case 1: cout << "You are walking. Next you can stand (0) or run (2)\n";
+                    state = (m_s)((rand()%2)*2); 
+                    break;
+            case 2: cout << "You are running. Next you can walk (1)\n";
+                    state = (m_s)1;
+                    break;
+            default:cout << "You are crawling. Next you can stand (0)\n";
+                    state = (m_s)0;
+                    break;
+        }
     }
 
 
