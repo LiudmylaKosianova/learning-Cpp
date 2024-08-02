61b0e1b80ca32fbe27dc867d63acab139696c981
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-08-02 11:15:23 +0200

    lecture 5 notes

---
+++ youtube/Nicholas_Day/lecture5/const.cpp
@@ -0,0 +1,19 @@
+/*
+DEFAULT CONSTRUCTOR
+
+class myClass{
+private:
+    int value;
+public:
+    myClass(){
+        value = 7;
+    }
+
+or it can be:
+    myClass(int v = 0){
+        value = v;
+    }
+};
+
+
+*/
