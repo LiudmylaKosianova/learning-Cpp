52874da3322d81c3cc05f09322d3bbe154cfd2e2
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-08-09 14:27:28 +0200

    lecture 10 exercise 2

--- youtube/Nicholas_Day/lecture10/multithread.cpp
+++ youtube/Nicholas_Day/lecture10/multithread.cpp
@@ -10,14 +10,32 @@ DWORD WINAPI BasicThread(LPVOID param){
     return 0;
 }
 
+DWORD WINAPI MessageThread(LPVOID param){
+    for(int i = 0; i < 5; i++){
+        cout << "Thread\n";
+        Sleep(500);
+    }
+    return 0;
+}
+
 
 int main(){
 
-    cout << "**********************************\n";
+    cout << "\n**********************************\n";
+    //HANDLE hdl;
+    // hdl = CreateThread(NULL, 0, BasicThread, NULL, 0, &threadid);
+    
+    LPVOID param = NULL;
     DWORD threadid;
-    HANDLE hdl;
-    hdl = CreateThread(NULL, 0, BasicThread, NULL, 0, &threadid);
-    system("pause");
+    HANDLE hdl[3];
+    int label = 1;
+    hdl[0] = CreateThread(NULL, 0, MessageThread, (LPVOID)label, 0, &threadid);
+    label = 2;
+    hdl[1] = CreateThread(NULL, 0, MessageThread, (LPVOID)label, 0, &threadid);
+    label = 3;
+    hdl[2] = CreateThread(NULL, 0, MessageThread, (LPVOID)label, 0, &threadid);
+
+    system("pause"); // will output "press any key to continue ..." to the screen
 
 
 
